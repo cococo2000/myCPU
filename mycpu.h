ececdfc672f12179815192677059807f19ad14f7
Nut-Guo <292998520@qq.com>
2020-10-15 17:21:16 +0800

    init prj

---
+++ mycpu.h
@@ -0,0 +1,14 @@
+`ifndef MYCPU_H
+    `define MYCPU_H
+
+    `define BR_BUS_WD       34
+    `define FS_TO_DS_BUS_WD 64
+    `define DS_TO_ES_BUS_WD 136
+    `define ES_TO_MS_BUS_WD 71
+    `define MS_TO_WS_BUS_WD 70
+    `define WS_TO_RF_BUS_WD 38
+    `define ES_FWD_BUS_WD   37
+    `define MS_FWD_BUS_WD   37
+    `define WS_FWD_BUS_WD   37
+    `define FWD_BUS_WD      111
+`endif
